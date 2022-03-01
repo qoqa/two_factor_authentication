41948c8eb3be2cac00cb4a47f2ad1e8fcf3b45ba
Jonathan Biolaz <jbiolaz@gmail.com>
2022-03-01 15:01:47 +0100

    use ActiveRecord::Base.writing_role

--- app/controllers/devise/two_factor_authentication_controller.rb
+++ app/controllers/devise/two_factor_authentication_controller.rb
@@ -4,8 +4,7 @@ class Devise::TwoFactorAuthenticationController < DeviseController
   prepend_before_action :authenticate_scope!
   before_action :prepare_and_validate, :handle_two_factor_authentication
 
-  def show
-  end
+  def show; end
 
   def update
     render :show and return if params[:code].nil?
@@ -18,14 +17,17 @@ class Devise::TwoFactorAuthenticationController < DeviseController
   end
 
   def resend_code
-    resource.send_new_otp
-    redirect_to send("#{resource_name}_two_factor_authentication_path"), notice: I18n.t('devise.two_factor_authentication.code_has_been_sent')
+    ActiveRecord::Base.connected_to(role: ActiveRecord::Base.writing_role, prevent_writes: false) do
+      resource.send_new_otp
+    end
+    redirect_to send("#{resource_name}_two_factor_authentication_path"),
+                notice: I18n.t('devise.two_factor_authentication.code_has_been_sent')
   end
 
   private
 
   def after_two_factor_success_for(resource)
-    set_remember_two_factor_cookie(resource)
+    remember_two_factor_cookie(resource)
 
     warden.session(resource_name)[TwoFactorAuthentication::NEED_AUTHENTICATION] = false
     # For compatability with devise versions below v4.2.0
@@ -36,29 +38,33 @@ class Devise::TwoFactorAuthenticationController < DeviseController
       sign_in(resource_name, resource, bypass: true)
     end
     set_flash_message :notice, :success
-    resource.update_attribute(:second_factor_attempts_count, 0)
+    ActiveRecord::Base.connected_to(role: ActiveRecord::Base.writing_role, prevent_writes: false) do
+      resource.update_attribute(:second_factor_attempts_count, 0)
+    end
 
     redirect_to after_two_factor_success_path_for(resource)
   end
 
-  def set_remember_two_factor_cookie(resource)
+  def remember_two_factor_cookie(resource)
     expires_seconds = resource.class.remember_otp_session_for_seconds
 
-    if expires_seconds && expires_seconds > 0
+    if expires_seconds && expires_seconds&.positive?
       cookies.signed[TwoFactorAuthentication::REMEMBER_TFA_COOKIE_NAME] = {
-          value: "#{resource.class}-#{resource.public_send(Devise.second_factor_resource_id)}",
-          expires: expires_seconds.seconds.from_now
+        value: "#{resource.class}-#{resource.public_send(Devise.second_factor_resource_id)}",
+        expires: expires_seconds.seconds.from_now
       }
     end
   end
 
-  def after_two_factor_success_path_for(resource)
+  def after_two_factor_success_path_for(_sresource)
     stored_location_for(resource_name) || :root
   end
 
   def after_two_factor_fail_for(resource)
     resource.second_factor_attempts_count += 1
-    resource.save
+    ActiveRecord::Base.connected_to(role: ActiveRecord::Base.writing_role, prevent_writes: false) do
+      resource.save
+    end
     set_flash_message :alert, :attempt_failed, now: true
 
     if resource.max_login_attempts?
@@ -75,6 +81,7 @@ class Devise::TwoFactorAuthenticationController < DeviseController
 
   def prepare_and_validate
     redirect_to :root and return if resource.nil?
+
     @limit = resource.max_login_attempts
     if resource.max_login_attempts?
       sign_out(resource)
